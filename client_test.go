7ab6c61863a3bfbb6bcbe1cfd520bdaaae6f2dcd
crossoverJie <crossoverjie@gmail.com>
2021-10-16 21:39:26 +0800

    :white_check_mark: Adding tests.

--- client_test.go
+++ client_test.go
@@ -7,8 +7,9 @@ import (
 
 func Test_client_Request(t *testing.T) {
 	httpClient := NewHttpClient("GET", "http://gobyexample.com", "")
-	err := httpClient.Request()
+	request, err := httpClient.Request()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(request)
 }
